5af9efef66decee3f1def685690f23ca19513b20
harshinivemula4 <harshinivemula164@gmail.com>
2025-11-02 20:23:31 +0530

    Update Events.jsx

--- src/assets/Events.jsx
+++ src/assets/Events.jsx
@@ -19,7 +19,7 @@ function Events() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/event", {
+      const response = await fetch("https://msd-backend-1.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -294,3 +294,4 @@ const events = [
 ];
 
 export default Events;
+
